2f27d5f5687c2c55fabce108f132f5141f2d4a8d
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-11 14:09:42 -0700

    route for '/hello' with HTML res

--- express_server.js
+++ express_server.js
@@ -11,9 +11,16 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// sending json data
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+
+// sending HTML
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
