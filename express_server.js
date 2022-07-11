bb330281f87429963eb2f66df36a11361d04cd67
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-11 15:14:47 -0700

    add _header.ejs to views

--- express_server.js
+++ express_server.js
@@ -1,20 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080; // default port 8080
 
 // add in EJS middlware
 app.set('view engine', 'ejs');
+app.use(morgan('dev'));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-// route to render "/urls" page
+// handle route parameters
 app.get('/urls/:id', (req, res) => {
   // data to pass to the ejs file
   let templateVars = {
@@ -24,6 +22,7 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+// route to render "/urls" page
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -32,6 +31,10 @@ app.get('/urls', (req, res) => {
 // sending json data
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => {
