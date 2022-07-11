9cd2f7a5cde12ce4f3be63c55c41e3ab90921e91
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-11 14:44:27 -0700

    add EJS middleware

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// add in EJS middlware
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -20,6 +23,18 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+// variable created in the function
+app.get('/set', (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+// trying to access the variable created in the function
+app.get('/fetch', (req, res) => {
+  res.send(`a = ${a}`);
+});
+// results in an error 'a is not defined'
 
 
 app.listen(PORT, () => {
