0ce443ee5daf4817cefb61d8bd4a5e23e39145d2
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-14 16:20:14 -0700

    login working

--- express_server.js
+++ express_server.js
@@ -16,7 +16,6 @@ app.use(cookieSession({
   keys: ['key'],
 }));
 //////////////// data /////////////////////
-// URL database
 const urlDatabase = {
   b6UTxQ: {
     longURL: 'https://www.tsn.ca',
@@ -42,10 +41,9 @@ const urlDatabase = {
     longURL: 'https://www.google.com/images',
     userID: 'a',
   }
-
 };
 
-// user database
+// object to store known users
 const users = {
   userRandomID: {
     id: 'userRandomID',
@@ -63,24 +61,25 @@ const users = {
     password: 'cw',
   }
 };
-//////////// helper functions //////////////
 
 const { generateRandomString, getUserByEmail, checkCookie, urlsForUser } = require('./helpers');
 
 //////////   GET ROUTES   //////////
+
 // GET for /register
 app.get('/register', (req, res) => {
   // if user is logged in and tries to access register page, redirect to /urls
   if (checkCookie(req, users)) {
     return res.redirect('/urls');
   }
+
+  // if user is not logged in, render register page
   const templateVars = {
     urls: urlDatabase,
     email: undefined,
-    message: undefined,
+    errorMessage: undefined,
   };
-  // if user is not logged in, render register page
-  res.render('register', templateVars);
+  return res.render('register', templateVars);
 });
 
 app.get('/login', (req, res) => {
@@ -91,7 +90,7 @@ app.get('/login', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
     email: undefined,
-    message: undefined
+    errorMessage: undefined
   };
   res.render('login', templateVars);
 });
@@ -105,7 +104,7 @@ app.get('/u/:id',(req, res) => {
     const templateVars = {
       id: req.params.id,
       email: undefined,
-      message: undefined
+      errorMessage: 'URL not found'
     };
     // render the generic error page
     return res.status(404).render('urls_notFound', templateVars);
@@ -130,6 +129,17 @@ app.get('/urls/new', (req, res) => {
 
 // GET for editing a URL
 app.get('/urls/:id', (req, res) => {
+  // if user is not logged in OR URL does not exist, OR URL does
+  // CHECK IN DATABSE if URL exists
+  if (!urlDatabase[req.params.id]) {
+    const templateVars = {
+      id: req.params.id,
+      email: undefined,
+      errorMessage: 'URL does not exist or you do not have access to edit it.'
+    };
+    res.status(404).render('urls_notFound', templateVars);
+  }
+
   // check to see if user is logged in/has cookie
   const cookie = checkCookie(req, users);
   //if user is not logged in
@@ -138,18 +148,9 @@ app.get('/urls/:id', (req, res) => {
     const templateVars = {
       id: req.params.id,
       email: undefined,
-      message: 'You must be logged in to view this page'
+      errorMessage: 'You must be logged in to view this page'
     };
     return res.status(401).render('login', templateVars);
-  }
-  // if user is not logged in OR URL does not exist, OR URL does
-  if (!urlDatabase[req.params.id] || urlDatabase[req.params.id].userID !== cookie.id) {
-    const templateVars = {
-      id: req.params.id,
-      email: undefined,
-      message: 'URL does not exist or you do not have access to edit it.'
-    };
-    res.status(404).render('urls_notFound', templateVars);
   }
 
   // check that short URL exists and userID in urlDatabase matches cookie id
@@ -183,21 +184,14 @@ app.get('/urls', (req, res) => {
 
 // home page route
 app.get('/', (req, res) => {
-  // if user is logged in, pass data with users object
-  if (checkCookie(req, users)) {
-    const userUrls = urlsForUser(req.session.userID, urlDatabase);
+  const userID = req.session.userID;
+  const userURLs = urlsForUser(userID, urlDatabase);
 
-    const templateVars = {
-      isLoggedIn: true,
-      urls: userUrls,
-      email: users[req.session.userID].email,
-      message: undefined,
-    };
-    // render page with data from users object
-    return res.render('urls_index', templateVars);
+  if (!userID) {
+    return res.redirect('/login');
+  } else {
+    return res.redirect('/urls');
   }
-  // if user is not logged in, redirect to login page
-  res.redirect('/login');
 });
 
 ///////////////// POST routes ///////////////
@@ -207,20 +201,18 @@ app.post('/register', (req, res) => {
 
   // check if email is empty string, 404 error
   if (req.body.email === '') {
-    console.log('email is empty: ', req.body.email);
     const templateVars = {
       email: undefined,
-      message: 'Email cannot be empty. Please enter a valid email.'
+      errorMessage: 'Email cannot be empty. Please enter a valid email.'
     };
     return res.status(400).render('register', templateVars);
   }
 
   // check if email is already in use
   if (getUserByEmail(req.body.email, users)) {
-    console.log('email is already in use: ', req.body.email);
     const templateVars = {
       email: undefined,
-      message: 'email is already in use. Please enter a different email.'
+      errorMessage: 'Email is already in use. Please enter a different email.'
     };
     return res.status(400).render('register', templateVars);
   }
@@ -243,20 +235,19 @@ app.post('/register', (req, res) => {
 // LOGIN POST route
 app.post('/login', (req, res) => {
   const user = getUserByEmail(req.body.email, users);
-  // iterate through users database to see if email matches
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  if (getUserByEmail(req.body.email, users) && bcrypt.compareSync(req.body.password, hashedPassword)) {
+
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
     // if email & password match, set cookie for user
     req.session.userID = user.id;
     // send user to /urls
     return res.redirect('/urls');
   }
+
   const templateVars = {
-    message: 'Email or password is incorrect. Try again.',
+    errorMessage: 'Email or password is incorrect. Try again.',
     email: undefined,
   };
   // if user with email can not be found, respond with 403
-  console.log(`email ${req.body.email} NOT found`);
   return res.status(403).render('login', templateVars);
 });
 
@@ -277,7 +268,6 @@ app.post('/urls', (req, res) => {
   if (!user) {
     return res.send('you are not logged in');
   }
-  console.log('user: ', user);
 
   const shortURL = generateRandomString();
   // add new shortURL to urlDatabase
@@ -286,25 +276,22 @@ app.post('/urls', (req, res) => {
     userID: req.session.userID
   };
   // redirect to new shortURL page
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 // DELETE POST route - delete a URL from the database
 app.post('/urls/:id/delete', (req, res) => {
   if (urlDatabase[req.params.id] === undefined) {
     // if short URL does not exist, redirect to urls_index page
-    console.log('short URL does not exist');
     return res.status(404).redirect('/urls');
   }
   // if user is not logged in, redirect to login page
   if (!checkCookie(req, users)) {
-    console.log('Please log in to delete a URL.');
     return res.status(403).redirect('/login');
   }
 
   // check if user has access to delete URL (own the URL)
   if (urlDatabase[req.params.id].userID !== req.session.userID) {
-    console.log('You do not have access to delete this URL.');
     return res.status(403).redirect('/urls');
   }
 
@@ -312,33 +299,29 @@ app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   // redirect to urls_index page
   return res.redirect('/urls');
-
 });
 
 // EDIT POST route to handle updates to long URL
 app.post('/urls/:id', (req, res) => {
   if (urlDatabase[req.params.id] === undefined) {
     // if short URL does not exist, redirect to urls_index page
-    console.log('URL does not exist.');
     return res.status(404).redirect('/urls');
   }
   // check if user logged in / has cookie
   if (!checkCookie(req, users)) {
     // if user is not logged in, redirect to login page
-    console.log('Please log in to edit a URL.');
     return res.status(401).redirect('/login');
   }
   // check if user has access to edit URL (own the URL)
   if (urlDatabase[req.params.id].userID !== req.session.userID) {
-    console.log('You do not have access to edit this URL.');
     return res.status(403).redirect('/urls');
   }
-  res.redirect(`/urls/${req.params.id}`);
+  return res.redirect(`/urls/${req.params.id}`);
 });
 
 // UPDATE POST route to handle updates to long URL
 // accessible from /urls/:id page, update btn
-app.post('/urls/:id/update', (req, res) => {
+app.post('/urls/:id', (req, res) => {
   // check if user logged in / has cookie
   if (!checkCookie(req, users)) {
     // if user is not logged in, redirect to login page
@@ -347,7 +330,7 @@ app.post('/urls/:id/update', (req, res) => {
   // update longURL in urlDatabase
   urlDatabase[req.params.id].longURL = req.body.longURL;
   // redirect to urls_index page
-  res.redirect(`/urls/${req.params.id}`);
+  return res.redirect(`/urls/${req.params.id}`);
 });
 
 
