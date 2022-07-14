5c1013a1d1c4c5cf65c4fcb5303c87ea17b26602
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-14 16:46:35 -0700

    more tests

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   'userRandomID': {
@@ -15,7 +15,7 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function() {
+describe('#getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail('user@example.com', testUsers);
     const expectedUserID = 'userRandomID';
@@ -28,5 +28,36 @@ describe('getUserByEmail', function() {
   it('should return undefined when it is provided an email that does not exist in the database', () => {
     const user = getUserByEmail('user2000@example.com', testUsers);
     assert.isUndefined(user, 'user is undefined');
+  });
+});
+
+const testUrls = {
+  abcd : {
+    longURL: 'http://www.google.com',
+    userID: 'userRandomID'
+  },
+  efgh : {
+    longURL: 'http://www.facebook.com',
+    userID: 'user2RandomID'
+  },
+  ijkl : {
+    longURL: 'http://www.instagram.com',
+    userID: 'userRandomID'
+  }
+};
+
+describe('#urlsForUser', () => {
+  it('should return the urls that are associated with a user', () => {
+    const urls = urlsForUser('userRandomID', testUrls);
+    const expectedUrls = {
+      abcd: 'http://www.google.com',
+      ijkl: 'http://www.instagram.com'
+    };
+    assert.deepEqual(urls, expectedUrls, 'urls are equal');
+  });
+
+  it('should return an empty object if the userID does not exist in user database', () => {
+    const urls = urlsForUser('doesNotExist', testUrls);
+    assert.deepEqual(urls, {}, 'urls are empty');
   });
 });
